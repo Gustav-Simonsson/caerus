8c7929c9e22238107ad8e33aecb0cd5b61cb6454
Gustav Simonsson <gustav.simonsson@gmail.com>
2013-12-11 23:34:48 +0100

    Remove single market view, complete view of "mytrades".

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-erl -pa deps/*/ebin ebin -I deps/*/include include -config priv/config/local.config -sname caerus_dev -s ca_app
+erl -pa deps/*/ebin ebin -I deps/*/include include -config priv/config/local.config -sname caerus_dev -s ca_app -s sync
